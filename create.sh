88d422ede165c8db596ba0307b8fc6eb5c897e50
Bruno Cruz <39481527+EnzoAkira@users.noreply.github.com>
2019-02-05 17:45:12 +0000

    Update create.sh

--- create.sh
+++ create.sh
@@ -14,8 +14,8 @@ git clone https://github.com/EnzoAkira/glpi
 
 git clone https://github.com/EnzoAkira/fusioninventory-for-glpi fusioninventory
 
-rm -rf glpi/.git glpi/install/install.php
-rm -rf fusioninventory/.git
+rm -rf glpi/.git glpi/install/install.php glpi/.atoum.php glpi/.circleci glpi/*.yml glpi/.tx glpi/tools glpi/tests glpi/
+rm -rf fusioninventory/.git fusioninventory/composer.json fusioninventory/*.yml fusioninventory/tools fusioninventory/screenshots fusioninventory/phpunit fusioninventory/.tx
 
 docker build -t enzoakira/glpi:$1 .
 docker push enzoakira/glpi:$1
