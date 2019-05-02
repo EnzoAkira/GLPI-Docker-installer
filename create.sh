1bfa79208f891cba3c200031db5079fdb72e5237
Bruno <bcruz@gocontact.pt>
2019-05-02 22:40:20 +0100

    Fix db connections

--- create.sh
+++ create.sh
@@ -7,20 +7,34 @@ if [ -z "$1" ]
     exit 1
 fi
 
-echo 'Introduce a password for Database'
+echo "Introduce a password for Database"
 read -sp 'Password Db: ' passvar
 
+if [ -z "$passvar" ]
+ then
+    echo "No argument supplied, default will be used"
+    $passvar = "admin"
+fi
+
+echo "Removing old glpi & fusioninventory"
 rm -rf glpi
 rm -rf fusioninventory
 
+echo "Cloning repos"
 git clone -b 9.4/gocontact --single-branch https://github.com/EnzoAkira/glpi
 
 git clone -b go-contact --single-branch https://github.com/EnzoAkira/fusioninventory-for-glpi fusioninventory
 
-rm -rf glpi/.git glpi/install/install.php glpi/.atoum.php glpi/.circleci glpi/*.yml glpi/.tx glpi/tools glpi/tests glpi/composer.json glpi/composer.lock
+echo "Cleaning repos"
+rm -rf glpi/.git glpi/.atoum.php glpi/.circleci glpi/*.yml glpi/.tx glpi/tools glpi/tests glpi/bin
 rm -rf fusioninventory/.git fusioninventory/composer.json fusioninventory/*.yml fusioninventory/tools fusioninventory/screenshots fusioninventory/phpunit fusioninventory/.tx
 
+echo "Rename install file"
+mv glpi/install/install.php glpi/install/install.php.bak
+
+echo "Modifying Db password"
 sed -i -e "s/Change-me/$passvar/" ./config_db.php
 
+echo "Build docker image"
 docker build -t enzoakira/glpi:$1 .
 docker push enzoakira/glpi:$1
